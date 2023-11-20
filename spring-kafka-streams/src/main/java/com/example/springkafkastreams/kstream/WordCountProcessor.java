ff54129523ff76aaa98c4e2ca2d5843f78338d34
Kannan Paramasivam <kannan.paramasivam@grainger.com>
2023-11-20 02:50:13 -0600

    named materialized views

---
+++ spring-kafka-streams/src/main/java/com/example/springkafkastreams/kstream/WordCountProcessor.java
@@ -0,0 +1,32 @@
+package com.example.springkafkastreams.kstream;
+
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+public class WordCountProcessor {
+    private static final Serde<String> stringSerDe = Serdes.String();
+
+    @Autowired
+    public void buildPipeline(StreamsBuilder streamsBuilder) {
+
+        KTable<String, Long> KTableWordCount = streamsBuilder
+                .stream("word-count-input", Consumed.with(stringSerDe, stringSerDe))
+                .mapValues(value -> value.toLowerCase())
+                .flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+                .peek((key, value) -> System.out.println("flat map values: " + "key: " + key + " value: " + value))
+                .groupBy((key, word) -> word, Grouped.with(stringSerDe, stringSerDe))
+                .count(Materialized.as("word-counts"));
+
+        KTableWordCount.toStream()
+                .peek((key, value) -> System.out.println("KTable values: " + "key: " + key + " value: " + value))
+                .to("word-count-output", Produced.with(stringSerDe, Serdes.Long()));
+    }
+}
