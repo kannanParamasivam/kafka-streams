36bd272bb94ebe93cf0d664343779e69133b7971
Kannan Paramasivam <kannan.paramasivam@grainger.com>
2023-11-20 00:39:20 -0600

    minimal input node

--- spring-kafka-streams/src/main/java/com/example/springkafkastreams/SpringKafkaStreamsApplication.java
+++ spring-kafka-streams/src/main/java/com/example/springkafkastreams/SpringKafkaStreamsApplication.java
@@ -2,8 +2,10 @@ package com.example.springkafkastreams;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 
 @SpringBootApplication
+@EnableKafkaStreams
 public class SpringKafkaStreamsApplication {
 
     public static void main(String[] args) {
